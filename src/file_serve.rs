005cc14d36aef06bf20ddf4130278144e77deb92
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2024-01-31 12:49:05 +0600

    Serve Dir

--- src/file_serve.rs
+++ src/file_serve.rs
@@ -1,9 +1,13 @@
 use std::path::PathBuf;
 
-use tower_http::services::ServeFile;
+use tower_http::services::{ServeDir, ServeFile};
 
-pub fn file_serve() -> ServeFile {
+pub fn serve_file() -> ServeFile {
     let path = PathBuf::from("src/main.rs");
     println!("Serving files from: {:?}", path);
     ServeFile::new(path)
+}
+
+pub fn serve_dir() -> ServeDir {
+    ServeDir::new("src")
 }
--- src/router.rs
+++ src/router.rs
@@ -1,10 +1,11 @@
-use crate::file_serve::file_serve;
+use crate::file_serve::{serve_dir, serve_file};
 use axum::{routing::get, Router};
 
 pub async fn routes() -> Router {
     Router::new()
         .route("/", get(root))
-        .route_service("/file", file_serve())
+        .route_service("/file", serve_file())
+        .nest_service("/files", serve_dir())
 }
 
 pub async fn root() -> Result<String, String> {
